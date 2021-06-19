942c5c000e53022860f80c82044cca375ba53d94
aliceyoo <jyynkr92@gmail.com>
2021-06-19 14:54:47 +0900

    refactor: trycatch 구조 추가

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -3,26 +3,34 @@ import { Todo } from 'store/Todo';
 
 export default function useTodo() {
   const getTodoList = () => {
-    const storage = window.localStorage.getItem('list');
-    const list = storage ? JSON.parse(storage) : [];
+    try {
+      const storage = window.localStorage.getItem('list');
+      const list = storage ? JSON.parse(storage) : [];
 
-    return { status: 200, data: list };
+      return { status: 200, data: list };
+    } catch (error) {
+      return { status: 400, data: [] };
+    }
   };
 
   const insertTodoItem = ({ content }: { content: string }) => {
-    const id = randomStr();
-    const storage = window.localStorage.getItem('list');
+    try {
+      const id = randomStr();
+      const storage = window.localStorage.getItem('list');
 
-    if (!storage) {
-      const list = [{ id, content, isComplete: false }];
-      window.localStorage.setItem('list', JSON.stringify(list));
+      if (!storage) {
+        const list = [{ id, content, isComplete: false }];
+        window.localStorage.setItem('list', JSON.stringify(list));
+        return { status: 200 };
+      }
+
+      const list = JSON.parse(storage) as Array<Todo>;
+      const todoItem = { id, content, isComplete: false };
+      window.localStorage.setItem('list', JSON.stringify(list.concat(todoItem)));
       return { status: 200 };
+    } catch (error) {
+      return { status: 400 };
     }
-
-    const list = JSON.parse(storage) as Array<Todo>;
-    const todoItem = { id, content, isComplete: false };
-    window.localStorage.setItem('list', JSON.stringify(list.concat(todoItem)));
-    return { status: 200 };
   };
 
   return {
